9874b88f569fe4b5e5add1382d98d5aa0ef392f4
koltunov <koltunov@protei.ru>
2025-06-20 04:31:15 +0300

    init telegraf module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TelegrafModule} from "nestjs-telegraf";
+import * as LocalSession from 'telegraf-session-local'
+
+const sessions = new LocalSession({database: 'session_db.json'})
 
 @Module({
-  imports: [],
+  imports: [
+      TelegrafModule.forRoot({
+        middlewares: [sessions.middleware()],
+        token: process.env.TELEGRAM_BOT_TOKEN as string,
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
